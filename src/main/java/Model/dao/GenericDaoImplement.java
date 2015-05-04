d7eadcb0f52b4f42fabd0e636dd7213cf2a0c580
zlot777 <zlot.dima@gmail.com>
2015-05-05 01:25:35 +0300

    commit

---
+++ src/main/java/Model/dao/GenericDaoImplement.java
@@ -0,0 +1,10 @@
+package Model.dao;
+
+import Model.dao.interfaces.GenericDao;
+
+/**
+ * Created by Администратор on 02.05.2015.
+ */
+public class GenericDaoImplement<T> implements GenericDao<T> {
+    
+}
--- src/main/java/Model/dao/interfaces/GenericDao.java
+++ src/main/java/Model/dao/interfaces/GenericDao.java
@@ -1,7 +1,14 @@
 package Model.dao.interfaces;
 
+import java.util.List;
+
 /**
  * Created by Администратор on 02.05.2015.
  */
-public interface GenericDao {
+public interface GenericDao <T> {
+  void create (T object);
+  void update (T object);
+  void delete (T object);
+  T findById(int id);
+  List<T> findAll();
 }
